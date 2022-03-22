57e2b949778e479f775c6fa71a3ad8f07820113f
Yang <49736123+s651794071@users.noreply.github.com>
2022-03-22 23:26:22 +0800

    添加评论

--- src/main/java/com/nowcoder/community/service/CommentService.java
+++ src/main/java/com/nowcoder/community/service/CommentService.java
@@ -2,16 +2,25 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.entity.Comment;
 import com.nowcoder.community.mapper.CommentMapper;
+import com.nowcoder.community.mapper.DiscussPostMapper;
+import com.nowcoder.community.util.CommunityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
 @Service
-public class CommentService {
+public class CommentService implements CommunityConstant {
 
     @Autowired
     private CommentMapper commentMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
 
     public List<Comment> findCommentByEntity(int entityType, int entityId, int offset, int limit) {
         return commentMapper.selectCommentByEntity(entityType, entityId, offset, limit);
@@ -19,5 +28,28 @@ public class CommentService {
 
     public int findCommentCount(int entityType, int entityId) {
         return commentMapper.selectCountByEntity(entityType,entityId);
+    }
+
+    // 增加评论业务
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public int addComment(Comment comment) {
+        if(comment == null)
+            throw new IllegalArgumentException("参数不能为空！");
+
+        // 添加评论
+
+        // 转义html标记
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        // 过滤敏感词
+        // comment.setContent(sensitiveFilter.filter(comment.getContent()));
+
+        int rows = commentMapper.insertComment(comment);
+        // 更新帖子的评论数量
+        if (comment.getEntityType() == ENTITY_TYPE_POST) { // 判断一下是帖子的评论还是评论的回复
+            int count = commentMapper.selectCountByEntity(ENTITY_TYPE_POST,comment.getEntityId());
+            discussPostMapper.updateCommentCount(comment.getEntityId(),count); // 第一个参数就是帖子的id，更新这个帖子下面的comment数量
+        }
+
+        return rows;
     }
 }
