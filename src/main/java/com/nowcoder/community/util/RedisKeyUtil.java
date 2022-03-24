416fdf85f2bfa95ac7e5d12fe53ea8800e970766
Yang <49736123+s651794071@users.noreply.github.com>
2022-03-24 15:12:57 +0800

    Create RedisKeyUtil.java
    为方便复用key开发的生成RedisKey的工具类

---
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -0,0 +1,15 @@
+package com.nowcoder.community.util;
+
+public class RedisKeyUtil {
+
+    private static final String SPLIT = ":";
+    private static final String PREFIX_ENTITY_LIKE = "like:entity";
+
+    // 某个实体的赞
+    // key:                            value:
+    // like:entity:entityType:entityId -> set(userId)
+    // 当我们需要统计点赞数量时， key直接调用set统计数量的方法，如果将来需要知道谁给我点赞，这些userId我们也可以从set里取到
+    public static String getEntityLike(int entityType, int entityId) {
+        return PREFIX_ENTITY_LIKE + SPLIT + entityType + SPLIT + entityId; 
+    }
+}
